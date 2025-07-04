1aefe2adb6133ca34bc506367bcae87dac7f6214
timeformoneyau <timeformoneyau@gmail.com>
2025-07-05 07:12:26 +0930

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,12 @@
-// firebase.js
+// firebase.js - Updated with password reset functionality
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import {
   getAuth,
   onAuthStateChanged,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
-  signOut as firebaseSignOut
+  signOut as firebaseSignOut,
+  sendPasswordResetEmail
 } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import {
   getFirestore,
@@ -45,10 +46,13 @@ export const auth = getAuth(app);
 export const db   = getFirestore(app);
 
 // Auth helpers
-export const signIn  = (email, password) => signInWithEmailAndPassword(auth, email, password);
-export const signUp  = (email, password) => createUserWithEmailAndPassword(auth, email, password);
+export const signIn = (email, password) => signInWithEmailAndPassword(auth, email, password);
+export const signUp = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 export const signOut = () => firebaseSignOut(auth);
-export const onAuth  = (cb) => onAuthStateChanged(auth, cb);
+export const onAuth = (cb) => onAuthStateChanged(auth, cb);
+
+// NEW: Password reset functionality
+export const resetPassword = (email) => sendPasswordResetEmail(auth, email);
 
 // Firestore exports
 export {
